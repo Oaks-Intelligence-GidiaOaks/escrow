e457fefc8b6c380e26f836897859ce5c9cb49f2e
Agbugba David C <57929456+Davidruph@users.noreply.github.com>
2023-10-12 11:54:01 +0100

    contact skeleton

--- src/assets/images/image.js
+++ src/assets/images/image.js
@@ -4,3 +4,6 @@ export const logo = `${PATH}/logo.svg`;
 export const facebook = `${PATH}/facebook.svg`;
 export const twitter = `${PATH}/twitter.svg`;
 export const linkedln = `${PATH}/linkedln.svg`;
+export const cardIcon1 = `${PATH}/cardIcon1.svg`;
+export const cardIcon2 = `${PATH}/cardIcon2.svg`;
+export const cardIcon3 = `${PATH}/cardIcon3.svg`;
--- src/page/ContacPage.jsx
+++ src/page/ContacPage.jsx
@@ -1,8 +1,56 @@
 import { Footer } from "../components";
+import { cardIcon1, cardIcon2, cardIcon3 } from "../assets";
+import Container from "../components/layout/container/Container";
 
 const ContacPage = () => {
   return (
     <>
+      <section className="contact-form-section">
+        <Container>
+          <div className="grid grid-flow-row md:grid-flow-col grid-cols-1 md:grid-cols-12 justify-center pt-10 pb-10 gap-y-4 gap-x-5">
+            <div className="py-2 col-span-12 md:col-span-6">
+              <div className="form-box"></div>
+            </div>
+            <div className="py-2 col-span-12 md:col-span-4">
+              <div className="card-contact mb-5">
+                <img
+                  src={cardIcon1}
+                  alt="cardIcon1"
+                  className="contact-card-icon"
+                />
+                <p className="contact-card-icon-head pt-2">General inquires</p>
+                <p className="contact-card-icon-title pt-2">
+                  customercare@escrow-tech.co.uk
+                </p>
+              </div>
+              <div className="card-contact mb-5">
+                <img
+                  src={cardIcon2}
+                  alt="cardIcon1"
+                  className="contact-card-icon"
+                />
+                <p className="contact-card-icon-head pt-2">
+                  Consultancy/Advisory
+                </p>
+                <p className="contact-card-icon-title pt-2">
+                  consultancy@escrow-tech.co.uk
+                </p>
+              </div>
+              <div className="card-contact mb-5">
+                <img
+                  src={cardIcon3}
+                  alt="cardIcon1"
+                  className="contact-card-icon"
+                />
+                <p className="contact-card-icon-head pt-2">Sales/Product</p>
+                <p className="contact-card-icon-title pt-2">
+                  sales@escrow-tech.co.uk
+                </p>
+              </div>
+            </div>
+          </div>
+        </Container>
+      </section>
       <Footer />
     </>
   );
