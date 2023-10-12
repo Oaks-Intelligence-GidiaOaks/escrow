0e2f900d0af0fc41ba2e369ec92a9a221ae6af64
Agbugba David C <57929456+Davidruph@users.noreply.github.com>
2023-10-12 08:36:24 +0100

    footer responsive

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -7,15 +7,19 @@ function Footer() {
     <>
       <section className="footer">
         <Container>
-          <div className="grid grid-flow-col auto-cols-max grid-cols-12 justify-center pt-10 pb-6 gap-1">
-            <div className=" clearfix col-span-12 sm:col-span-12 md:col-span-6 lg:col-span-3">
-              <img src={logo} alt="logo" className="pb-2" />
+          <div className="grid grid-flow-row md:grid-flow-col grid-cols-1 md:grid-cols-12 justify-center pt-10 pb-6 gap-y-4">
+            <div className="text-center py-2 col-span-12 md:col-span-3 md:text-left">
+              <img
+                src={logo}
+                alt="logo"
+                className="pb-2 mx-auto block md:mx-0 md:inline"
+              />
               <p className="footer-header pb-8 mb-5">Escrow-Tech</p>
               <a href="#" className="btn footer-quote mb-5">
                 Get a quote
               </a>
             </div>
-            <div className=" col-span-12 sm:col-span-12 md:col-span-6 lg:col-span-3">
+            <div className="text-center py-2 col-span-12 md:col-span-3 md:text-left">
               <p className="footer-head pb-2">Company</p>
               <Link to="#" className="footer-links pb-1">
                 About Us
@@ -33,7 +37,7 @@ function Footer() {
                 Careers
               </Link>
             </div>
-            <div className=" col-span-12 sm:col-span-12 md:col-span-6 lg:col-span-3">
+            <div className="text-center py-2 col-span-12 md:col-span-3 md:text-left">
               <p className="footer-head pb-2">Quick Links</p>
               <Link to="#" className="footer-links pb-1">
                 Home
@@ -57,7 +61,7 @@ function Footer() {
                 Become a partner
               </Link>
             </div>
-            <div className=" col-span-12 sm:col-span-6 md:col-span-3 lg:col-span-3">
+            <div className="text-center py-2 col-span-12 md:col-span-3 md:text-left">
               <p className="footer-head pb-2">Specialization</p>
               <Link to="#" className="footer-links pb-1">
                 Artificial Intelligence
@@ -76,15 +80,13 @@ function Footer() {
               </Link>
             </div>
           </div>
-          <div className="grid grid-cols-12 justify-center pt-5 pb-5">
-            <div className=" col-span-12 sm:col-span-6 md:col-span-6 lg:col-span-6 flex items-center">
-              <img src={facebook} alt="" className="px-3" />
-              <img src={twitter} alt="" className="py-3 px-3" />
-              <img src={linkedln} alt="" className="px-3" />
+          <div className="flex justify-between items-center pt-5 pb-5">
+            <div className="flex items-center">
+              <img src={facebook} alt="facebook link" className="px-3" />
+              <img src={twitter} alt="twitter link" className="py-3 px-3" />
+              <img src={linkedln} alt="linkedln link" className="px-3" />
             </div>
-            <div className=" col-span-12 sm:col-span-6 md:col-span-6 lg:col-span-6 flex items-center justify-end copyright">
-              ©2023 Escrow-Tech Limited
-            </div>
+            <div className="copyright">©2023 Escrow-Tech Limited</div>
           </div>
         </Container>
       </section>
