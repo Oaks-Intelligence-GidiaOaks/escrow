b1f5d64c06591d7d05d6ba423dfa518be8ecaf9d
Agbugba David C <57929456+Davidruph@users.noreply.github.com>
2023-10-12 08:16:18 +0100

    footer component

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -1,37 +1,93 @@
 import { logo, facebook, twitter, linkedln } from "../../../assets";
+import { Link } from "react-router-dom";
 import Container from "../container/Container";
 
 function Footer() {
   return (
     <>
-      <Container>
-        <section className="footer">
-          <div className="container">
-            <div className="row justify-content-center">
-              <div className="col-lg-3">
-                <img
-                  src={logo}
-                  alt="Logo"
-                  className="img-fluid nav-brand pb-2"
-                />
-                <h5 className="footer-header">Escrow-Tech</h5>
-                <a href="#" className="btn footer-quote">
-                  Get a quote
-                </a>
-              </div>
-              <div className="col-lg-3 ">
-                <img src={logo} alt="Logo" className="img-fluid nav-brand" />
-              </div>
-              <div className="col-lg-3">
-                <img src={logo} alt="Logo" className="img-fluid nav-brand" />
-              </div>
-              <div className="col-lg-3">
-                <img src={logo} alt="Logo" className="img-fluid nav-brand" />
-              </div>
+      <section className="footer">
+        <Container>
+          <div className="grid grid-flow-col auto-cols-max grid-cols-12 justify-center pt-10 pb-6 gap-1">
+            <div className=" clearfix col-span-12 sm:col-span-12 md:col-span-6 lg:col-span-3">
+              <img src={logo} alt="logo" className="pb-2" />
+              <p className="footer-header pb-8 mb-5">Escrow-Tech</p>
+              <a href="#" className="btn footer-quote mb-5">
+                Get a quote
+              </a>
+            </div>
+            <div className=" col-span-12 sm:col-span-12 md:col-span-6 lg:col-span-3">
+              <p className="footer-head pb-2">Company</p>
+              <Link to="#" className="footer-links pb-1">
+                About Us
+              </Link>
+              <br />
+              <Link to="#" className="footer-links pb-1">
+                Terms of Service
+              </Link>
+              <br />
+              <Link to="#" className="footer-links pb-1">
+                Privacy Policy
+              </Link>
+              <br />
+              <Link to="#" className="footer-links pb-1">
+                Careers
+              </Link>
+            </div>
+            <div className=" col-span-12 sm:col-span-12 md:col-span-6 lg:col-span-3">
+              <p className="footer-head pb-2">Quick Links</p>
+              <Link to="#" className="footer-links pb-1">
+                Home
+              </Link>
+              <br />
+              <Link to="#" className="footer-links pb-1">
+                Services
+              </Link>
+              <br />
+              <Link to="#" className="footer-links pb-1">
+                Contact
+              </Link>
+              <br />
+              <br />
+              <p className="footer-head pb-2">Partners</p>
+              <Link to="#" className="footer-links pb-2">
+                See partners
+              </Link>
+              <br />
+              <Link to="#" className="footer-links pb-1">
+                Become a partner
+              </Link>
+            </div>
+            <div className=" col-span-12 sm:col-span-6 md:col-span-3 lg:col-span-3">
+              <p className="footer-head pb-2">Specialization</p>
+              <Link to="#" className="footer-links pb-1">
+                Artificial Intelligence
+              </Link>
+              <br />
+              <Link to="#" className="footer-links pb-1">
+                Deep Learning
+              </Link>
+              <br />
+              <Link to="#" className="footer-links pb-1">
+                Natural Language processing
+              </Link>
+              <br />
+              <Link to="#" className="footer-links pb-1">
+                Image Recognition
+              </Link>
             </div>
           </div>
-        </section>
-      </Container>
+          <div className="grid grid-cols-12 justify-center pt-5 pb-5">
+            <div className=" col-span-12 sm:col-span-6 md:col-span-6 lg:col-span-6 flex items-center">
+              <img src={facebook} alt="" className="px-3" />
+              <img src={twitter} alt="" className="py-3 px-3" />
+              <img src={linkedln} alt="" className="px-3" />
+            </div>
+            <div className=" col-span-12 sm:col-span-6 md:col-span-6 lg:col-span-6 flex items-center justify-end copyright">
+              ©2023 Escrow-Tech Limited
+            </div>
+          </div>
+        </Container>
+      </section>
     </>
   );
 }
