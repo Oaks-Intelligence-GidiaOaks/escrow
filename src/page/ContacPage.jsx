014327370ca6f7704b67d08e4980fa9b2696c613
Agbugba David C <57929456+Davidruph@users.noreply.github.com>
2023-10-12 12:40:14 +0100

    contact update

--- src/page/ContacPage.jsx
+++ src/page/ContacPage.jsx
@@ -8,10 +8,73 @@ const ContacPage = () => {
       <section className="contact-form-section">
         <Container>
           <div className="grid grid-flow-row md:grid-flow-col grid-cols-1 md:grid-cols-12 justify-center pt-10 pb-10 gap-y-4 gap-x-5">
-            <div className="py-2 col-span-12 md:col-span-6">
-              <div className="form-box"></div>
+            <div className="py-2 col-span-12 md:col-span-6 lg:col-span-7">
+              <div className="form-box">
+                <p className="card-form-head pb-1">Keep In Touch</p>
+                <p className="card-form-title mb-5">
+                  We are easily reachable through our contact form or email
+                </p>
+                <div className="contact-form-inputs mb-5">
+                  <label
+                    htmlFor="Name"
+                    className="contact-form-label block mb-2"
+                  >
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    className="contact-form-input block w-full"
+                    placeholder="Your name"
+                  />
+                </div>
+                <div className="contact-form-inputs mb-5">
+                  <label
+                    htmlFor="Name"
+                    className="contact-form-label block mb-2"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    className="contact-form-input block w-full"
+                    placeholder="youremail@example.com"
+                  />
+                </div>
+                <div className="contact-form-inputs mb-5">
+                  <label
+                    htmlFor="Subject"
+                    className="contact-form-label block mb-2"
+                  >
+                    Subject
+                  </label>
+                  <input
+                    type="text"
+                    className="contact-form-input block w-full"
+                    placeholder="E.g (Quote to build to a trading platform)"
+                  />
+                </div>
+                <div className="contact-form-inputs mb-5">
+                  <label
+                    htmlFor="Subject"
+                    className="contact-form-label block mb-2"
+                  >
+                    Message
+                  </label>
+                  <textarea
+                    type="text"
+                    className="contact-form-textarea block w-full"
+                    placeholder="E.g (Quote to build to a trading platform)"
+                  ></textarea>
+                </div>
+
+                <div className="contact-form-inputs mb-4">
+                  <button className="contact-form-submit-btn w-full">
+                    Submit
+                  </button>
+                </div>
+              </div>
             </div>
-            <div className="py-2 col-span-12 md:col-span-4">
+            <div className="py-2 col-span-12 md:col-span-3 lg:col-span-4">
               <div className="card-contact mb-5">
                 <img
                   src={cardIcon1}
